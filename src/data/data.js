2d64f2a2ee9db8423bbd6e613765a209b77e67d7
marcserranog <marcserranog@gmail.com>
2025-02-03 03:37:32 +0100

    Refactoring data to a properly class

---
+++ src/data/data.js
@@ -0,0 +1,50 @@
+
+export const personalInfo = {
+    name: "Marc Serrano",
+    title: "Software Engineer",
+    email: "marcserranog@gmail.com",
+    phone: "+34 665 537 096",
+    location: "Barcelona, Spain",
+    linkedin: "https://www.linkedin.com/in/marcserranog",
+    github: "https://github.com/marcserranog",
+  };
+  
+  export const projects = [
+    {
+      name: "Haircurt Board",
+      description:
+        "Platform for managing aesthetic services with client management, reservations, and invoicing",
+      technologies: [
+        "Java",
+        "Spring Framework",
+        "MySQL",
+        "Angular",
+        "TypeScript",
+      ],
+    },
+    {
+      name: "Frutikas",
+      description:
+        "Subscription-based web platform for monthly product delivery",
+      technologies: [
+        "React",
+        "JavaScript",
+        "CSS",
+        "Java",
+        "Spring Framework",
+        "MySQL",
+      ],
+    },
+    {
+      name: "Transports TCM",
+      description:
+        "Project of the university, which consists of a company of transport of frozen food, it has to develop the calculations that makes the trucks depending on the price, the distance that it travels looking for the most optimal solution. ",
+      technologies: [
+        "Java",
+        "Spring Framework",
+        "MySQL",
+      ],
+      url: "https://github.com/marcserranog/transports-tcm",
+    },
+  ];
+  
