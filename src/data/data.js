566adfa3ea3a1ade47f8486133e659bc97a66114
marcserranog <marcserranog@gmail.com>
2025-02-05 00:43:08 +0100

    modyfing data and the cv

--- src/data/data.js
+++ src/data/data.js
@@ -10,30 +10,34 @@ export const personalInfo = {
   };
   
   export const projects = [
+
     {
-      name: "Haircurt Board",
+      name: "FAKEFLIX",
       description:
-        "Platform for managing aesthetic services with client management, reservations, and invoicing",
-      technologies: [
-        "Java",
-        "Spring Framework",
-        "MySQL",
-        "Angular",
-        "TypeScript",
-      ],
-    },
-    {
-      name: "Frutikas",
-      description:
-        "Subscription-based web platform for monthly product delivery",
+        "FAKEFLIX is a business case developed in 3 days for a company. It lets users browse movies, view details, and add favorites to a wishlist, fetching data from TheMovieDatabase API with a responsive and dynamic UI.",
       technologies: [
         "React",
         "JavaScript",
-        "CSS",
+        "Tailwind CSS",
+        "Node.js",
+        "Webpack",
+        "Jest",
+      ],
+      url: "https://github.com/marcserranog/business-case-frontend-marc-serrano",
+    },
+    {
+      name: "HairBoard",
+      description:
+        "This was my final degree project at university. A platform for managing aesthetic services. An administrator controls clients, bookings and invoicing, improving efficiency and productivity by automating processes.",
+      technologies: [
         "Java",
         "Spring Framework",
+        "Angular",
+        "Node.js",
+        "TypeScript",
         "MySQL",
       ],
+      url: "https://github.com/marcserranog/TFG_HairBoard"
     },
     {
       name: "Transports TCM",
